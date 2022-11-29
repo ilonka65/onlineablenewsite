eea18531ca958d092126636541403e877fe7d21d
manishworkholics <manishworkholics@gmail.com>
2022-11-29 17:31:35 +0530

    new code pushed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="container-fluid mt-3">
+         <div className="row">
+          <div className="col p-3 "></div>
+          <div className="col p-3 "><h1>Hello home page</h1></div>
+          <div className="col p-3 "></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
